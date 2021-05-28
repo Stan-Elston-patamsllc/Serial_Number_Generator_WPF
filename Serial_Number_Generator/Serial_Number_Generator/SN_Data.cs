4fadcbddb1a4c3a25f4aad39fbf948148873f99a
Stan-Elston-patamsllc <80419842+Stan-Elston-patamsllc@users.noreply.github.com>
2021-05-28 14:31:10 -0400

    Fixed Missing Files

---
+++ Serial_Number_Generator/Serial_Number_Generator/SN_Data.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Serial_Number_Generator
+{
+    public class SN_Data
+    {
+        public SN_Data()
+        {
+        }
+
+        public void Set_Data(Guid id, DateTime date, string kitid, int qty, string sn_start,
+            string sn_finish, int week, string rev, string customer, string assembly, string po,
+            bool tin_lead, bool lead_free, bool halogen_free, bool no_clean, bool conformal_coated,
+            bool class_three, string full_sn_finish, string full_sn_start, bool print_traveler, bool print_label,
+            bool customer_sn)
+        {
+            ID = id;
+            Date = date;
+            Kit_ID = kitid;
+            QTY = qty;
+            SN_Start = sn_start;
+            SN_Finish = sn_finish;
+            Week = week;
+            Rev = rev;
+            Customer = customer;
+            Assembly = assembly;
+            PO = po;
+            Tin_Lead = tin_lead;
+            Lead_Free = lead_free;
+            Halogen_Free = halogen_free;
+            No_Clean = no_clean;
+            Conformal_Coated = conformal_coated;
+            Class_Three = class_three;
+            Full_SN_Finish = full_sn_finish;
+            Full_SN_Start = full_sn_start;
+            Print_Label = print_label;
+            Print_Traveler = print_traveler;
+            Customer_SN = customer_sn;
+        }
+
+        public static bool Print_Traveler { get; set; }
+        public static bool Print_Label { get; set; }
+        public static DateTime Date { get; set; }
+        public static Guid ID { get; set; }
+        public static string Kit_ID { get; set; }
+        public static int QTY { get; set; }
+        public static string SN_Finish { get; set; }
+        public static string SN_Start { get; set; }
+        public static int Week { get; set; }
+        public static string Rev { get; set; }
+        public static string Customer { get; set; }
+        public static string Assembly { get; set; }
+        public static string PO { get; set; }
+        public static bool Tin_Lead { get; set; }
+        public static bool Lead_Free { get; set; }
+        public static bool Halogen_Free { get; set; }
+        public static bool Conformal_Coated { get; set; }
+        public static bool Class_Three { get; set; }
+        public static bool No_Clean { get; set; }
+        public static string Full_SN_Finish { get; set; }
+        public static string Full_SN_Start { get; set; }
+        public static bool Customer_SN { get; set; }
+    }
+}
---
+++ Serial_Number_Generator/Serial_Number_Generator/Sql_Query.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Serial_Number_Generator
+{
+    internal class Sql_Query
+    {
+        private static Dictionary<Type, SqlDbType> typeMap;
+
+        public void sql_data(string sql_connection, string sql_command, Dictionary<string, object> SQL_Parameters)
+        {
+            SqlConnection sql_InsertData = new SqlConnection(sql_connection);
+
+            SqlCommand cmd = new SqlCommand(sql_command, sql_InsertData);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            sql_InsertData.Open();
+
+            foreach (var item in SQL_Parameters)
+            {
+                try
+                {
+                    cmd.Parameters.Add(new SqlParameter(item.Key, GetDbType(item.Value.GetType())));
+                    cmd.Parameters[item.Key].Value = item.Value;
+                }
+                catch
+                {
+                    cmd.Parameters.Add(new SqlParameter(item.Key, DBNull.Value));
+                    cmd.Parameters[item.Key].Value = DBNull.Value;
+                }
+            }
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public DataTable Sql_Select(string sql_connection, string sql_command, Dictionary<string, object> SQL_Parameters = null)
+        {
+            SqlConnection sql_QueryData = new SqlConnection(sql_connection);
+
+            SqlCommand cmd = new SqlCommand(sql_command, sql_QueryData)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            if (SQL_Parameters != null)
+            {
+                foreach (var item in SQL_Parameters)
+                {
+                    cmd.Parameters.Add(new SqlParameter(item.Key, item.Value));
+                }
+            }
+
+            sql_QueryData.Open();
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+            dataAdapter.Fill(dataTable);
+            sql_QueryData.Close();
+
+            return dataTable;
+        }
+
+        public static SqlDbType GetDbType(Type giveType)
+        {
+            typeMap = new Dictionary<Type, SqlDbType>();
+
+            typeMap[typeof(string)] = SqlDbType.NVarChar;
+            typeMap[typeof(char[])] = SqlDbType.NVarChar;
+            typeMap[typeof(byte)] = SqlDbType.TinyInt;
+            typeMap[typeof(short)] = SqlDbType.SmallInt;
+            typeMap[typeof(int)] = SqlDbType.Int;
+            typeMap[typeof(long)] = SqlDbType.BigInt;
+            typeMap[typeof(byte[])] = SqlDbType.Image;
+            typeMap[typeof(bool)] = SqlDbType.Bit;
+            typeMap[typeof(DateTime)] = SqlDbType.DateTime2;
+            typeMap[typeof(DateTimeOffset)] = SqlDbType.DateTimeOffset;
+            typeMap[typeof(decimal)] = SqlDbType.Money;
+            typeMap[typeof(float)] = SqlDbType.Real;
+            typeMap[typeof(double)] = SqlDbType.Float;
+            typeMap[typeof(TimeSpan)] = SqlDbType.Time;
+            typeMap[typeof(Guid)] = SqlDbType.UniqueIdentifier;
+
+            // Allow nullable types to be handled
+            giveType = Nullable.GetUnderlyingType(giveType) ?? giveType;
+
+            if (typeMap.ContainsKey(giveType))
+            {
+                return typeMap[giveType];
+            }
+
+            throw new ArgumentException($"{giveType.FullName} is not a supported .NET class");
+        }
+    }
+}
