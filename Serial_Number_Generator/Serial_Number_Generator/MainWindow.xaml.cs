958202e91f69c322496a7abea358ed7b315bb04b
Stan-Elston-patamsllc <80419842+Stan-Elston-patamsllc@users.noreply.github.com>
2021-05-28 14:27:16 -0400

    Add files via upload

---
+++ Serial_Number_Generator/Serial_Number_Generator/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Serial_Number_Generator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Constants
+
+        public const string DATABASE = "PAS_Logs";
+
+        //SQL Table
+        public const string SERVER = "PAS-SQL";
+
+        public const string SQL_CONNECTION = "Server=" + SERVER +
+                              ";Database=" + DATABASE +
+                              ";Integrated Security=True;";
+
+        public const string GET_KIT_IDS = "SP_Get_Kit_IDs";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillComboBox();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        public void FillComboBox()
+        {
+            Sql_Query query = new Sql_Query();
+            DataTable dataTable = query.Sql_Select(SQL_CONNECTION, GET_KIT_IDS);
+
+            Kit_ID_Combobox.ItemsSource = dataTable.AsDataView();
+        }
+    }
+}
